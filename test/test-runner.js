023697a9e293676a9fedb83917d96ff1449a4f64
Mark McKelvy <mmckelvy@gmail.com>
2016-05-08 22:05:25 -0700

    parallel

--- test/test-runner.js
+++ test/test-runner.js
@@ -4,5 +4,7 @@ const path = require('path');
 const testDir = path.join(process.cwd(), 'test');
 
 const testRunSeries = require(path.join(testDir, 'run-series.js'));
+const testRunParallel = require(path.join(testDir, 'run-parallel.js'));
 
 testRunSeries();
+testRunParallel();
